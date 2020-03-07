76e0da3ed815f1b9cf3f30b9ccf19b6ebff34881
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2020-03-07 17:40:51 +0000

    character moving

---
+++ DJCO1/Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    const float Move_Delta = 0.01f;    // Place at top of script - change to whatever value is comfortable
+
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> ();    
+        animator = GetComponent<Animator> ();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis ("Horizontal");
+
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        } 
+        
+             // If we have movement, set our flip - else keep our last flipped state
+         if (Mathf.Abs(move.x) > Move_Delta)
+         {
+             // this true false may need to be reversed - If its flipping the wrong way, change them the other way.
+             spriteRenderer.flipX = move.x > Move_Delta ? false : true;
+         }
+
+        /*bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite) 
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }*/
+
+        animator.SetBool ("grounded", grounded);
+        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}
