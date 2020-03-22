85d2b6a294535ddd6ce26c1cf87e723c6a8c00af
zmartz <36684046+zmartz@users.noreply.github.com>
2020-03-22 04:43:27 +0000

    Layout finished

---
+++ DJCO1/Assets/Scripts/PlayOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnCollision : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+    }
+}
